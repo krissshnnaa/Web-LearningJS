fe32a9f2ff0144ac1acd60b917353941fd5317fb
krissshnnaa <krishnasharma413@gmail.com>
2021-09-07 12:15:20 +0530

    successfull

---
+++ Functions/Function1.js
@@ -0,0 +1,21 @@
+// Write a JS Program to check two numbers and return true if one of the number is 100 or the sum of the two numbers is 100
+
+/*function isEqualTo100(a,b){
+    if(a===100 || b===100 || a+b>=100){
+ console.log(true);
+    }
+  else{
+    console.log(false);
+}
+*/
+const isEqualTo100=(a,b) => a===100 || b===100 ||(a+b)===100; //for arrow operator 
+console.log(isEqualTo100(100,0));;
+console.log(isEqualTo100(0,100));;
+
+console.log(isEqualTo100(10,0));;
+console.log(isEqualTo100(0,10));;
+
+console.log(isEqualTo100(20,80));;
+console.log(isEqualTo100(50,50));;
+
+
